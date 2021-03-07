4ab2e0188a0a763275a076cc149c957a509e1d3a
Judex66 <78801062+Judex66@users.noreply.github.com>
2021-03-07 13:23:22 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,12 +1,31 @@
 
 exports.min = function min (array) {
-  return 0;
-}
+if (array.length ==0){ return 0;
+ }
+  else{
+ return Math.min.apply(Math, array);
+  }
+  }
+
 
 exports.max = function max (array) {
-  return 0;
+if (array.length ==0){ return 0;
+ }
+ else{
+return Math.max.apply(Math, array);
+}
 }
 
 exports.avg = function avg (array) {
-  return 0;
+if (array.length ==0){ return 0;
+ }
+ else{
+ var arr1=0;
+ var res=0;
+  for (var i=0;i<array.length;i++){
+      arr1+=array[i];
+      }
+      res=arr1/array.length;
+  return res;
+}
 }
